f0aeada85101b3f5cba7f9bb050fc274c5fdd632
rileyesther <riley.anson1@gmail.com>
2024-09-02 12:33:47 +0200

    first

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+expandButton.addEventListener('click', expand) 
+// verteld de knop dat als erop geklikt word het moet vergroten
 
 function expand () {
   document.body.classList.toggle('expand')
