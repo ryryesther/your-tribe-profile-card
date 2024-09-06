d498d30d176acb25efa84bee96f1fccb401a3085
rileyesther <riley.anson1@gmail.com>
2024-09-06 09:46:28 +0200

    yayy

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,20 @@ expandButton.addEventListener('click', expand)
 function expand () {
   document.body.classList.toggle('expand')
 }
+
+document.addEventListener('click', (e) => {
+  createFloatingHeart(e.clientX, e.clientY);
+});
+
+function createFloatingHeart(x, y) {
+  const heart = document.createElement('div');
+  heart.classList.add('floating-heart');
+  heart.style.left = `${x}px`;
+  heart.style.top = `${y}px`;
+  document.body.appendChild(heart);
+
+  
+  setTimeout(() => {
+    heart.remove();
+  }, 2000); 
+}
